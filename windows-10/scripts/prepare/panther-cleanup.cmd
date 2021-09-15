c7e5cc8c2c567995c95792828e7c7b57e363ab77
Robert Socha <socha@socha.it>
2021-09-15 05:20:19 +0200

    cleanup fix

--- windows-10/scripts/prepare/panther-cleanup.cmd
+++ windows-10/scripts/prepare/panther-cleanup.cmd
@@ -5,6 +5,6 @@ rmdir /s /q \Windows\Panther\actionqueue
 del /f /q \Windows\Panther\unattend.xml
 del /f /q \Windows\Panther\setupinfo
 del /f /q \Windows\Panther\setup.etl
-del /f /q \Windows\Panther\MainQueueOnline0.que
-del /f /q *.log
-del /f /q Contents*
+del /f /q \Windows\Panther\*.que
+del /f /q \Windows\Panther\*.log
+del /f /q \Windows\Panther\Contents*
