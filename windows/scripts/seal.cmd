32c04fe4be8b7bec0550efb02c85741304e950f6
Robert Socha <socha@socha.it>
2021-09-15 21:54:42 +0200

    .

--- windows/scripts/seal.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-reg import clear-swap.reg
-powershell Rename-Computer -NewName TEMPLATE -Force
-del result.txt
-c:\windows\system32\sysprep\sysprep /generalize /shutdown /oobe /mode:vm /unattend:C:\Windows\setup\scripts\unattend.xml
